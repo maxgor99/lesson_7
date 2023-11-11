9318a0257d2359d609b931f97fdae57e6eadad2d
Max G <maxgor99@gmail.com>
2023-11-11 12:15:09 +0300

    hw lesson_7

---
+++ src/test/java/test/PracticeFormTest.java
@@ -0,0 +1,92 @@
+package test;
+
+
+import org.junit.jupiter.api.Test;
+import pages.PracticeForm;
+
+
+
+
+
+
+
+public class PracticeFormTest extends BaseTest {
+    PracticeForm practiceForm = new PracticeForm();
+
+
+    @Test
+    void successPracticeFormTest() {
+        practiceForm.OpenPage().
+                setFirstName("Max").
+                lastNameInput("Gor").
+                userEmailInput("maxgor@gmail.com").
+                userNumberInput("1122334455").
+                userCurrentAdressInput("text text text").
+                genderWrapper("Male").
+                setDateOfBirth("01","October","1989").
+                subjectInput("English").
+                hobbiesWrapper("Sports").
+                hobbiesWrapper("Music").uploadPicture("frost.jpeg").
+                selectState("NCR").
+                selectCity("Delhi").
+                pressSubmit();
+
+
+        practiceForm.checkResult("Student Name", "Max Gor")
+                .checkResult("Student Email", "maxgor@gmail.com")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "1122334455")
+                .checkResult("Date of Birth", "01 October,1989")
+                .checkResult("Subjects", "English")
+                .checkResult("Hobbies", "Sports, Music")
+                .checkResult("Picture", "frost.jpeg")
+                .checkResult("Address", "text text text")
+                .checkResult("State and City", "NCR Delhi");
+
+
+    }
+
+
+    @Test
+    void checkRequiredFieldsTest() {
+        practiceForm.OpenPage().
+                setFirstName("Max").
+                lastNameInput("Gor").
+                userNumberInput("1122334455").
+                genderWrapper("Male").
+                setDateOfBirth("01","October","1989").
+                pressSubmit();
+
+        practiceForm.checkResult("Student Name", "Max Gor")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "1122334455")
+                .checkResult("Date of Birth", "01 October,1989");
+
+
+
+    }
+    @Test
+    void negativePracticeFormTest() {
+        practiceForm.OpenPage().
+                setFirstName("Max").
+                lastNameInput("Gor").
+                userEmailInput("maxgor@gmail.com").
+                userNumberInput("aabbcceedd").
+                userCurrentAdressInput("text text text").
+                genderWrapper("Male").
+                setDateOfBirth("01","October","1989").
+                subjectInput("English").
+                hobbiesWrapper("Sports").
+                hobbiesWrapper("Music").uploadPicture("frost.jpeg").
+                selectState("NCR").
+                selectCity("Delhi").
+                pressSubmit();
+
+
+        practiceForm.formResult();
+
+
+    }
+
+}
+
